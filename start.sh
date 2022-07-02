3ccc18d0111aaf24a75aad7e74443cadb1053873
Pshsabet <76724911+Pshsabet@users.noreply.github.com>
2022-07-02 11:31:10 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 cd /app
 echo "----- Now trying to run gunicorn ------ " 
-CMD gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app 
+gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
